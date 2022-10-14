fa1950988958b7872eb7f66c1a6e753dc7c2a11a
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-14 17:46:47 -0400

    Added logic to viewss file for user to POST and DELETE objects

--- backend/watchlist/views.py
+++ backend/watchlist/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -7,12 +8,22 @@ from .serializers import WatchlistSerializer
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 @permission_classes([IsAuthenticated])
-def user_watchlist(request):
+def user_watchlist(request, pk):
+    watchlist = get_object_or_404(Watchlist, pk=pk)
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
     if request.method == 'GET':
         stocks = Watchlist.objects.filter(user_id=request.user.id)
         serializer = WatchlistSerializer(stocks, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = WatchlistSerializer(watchlist, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        watchlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
