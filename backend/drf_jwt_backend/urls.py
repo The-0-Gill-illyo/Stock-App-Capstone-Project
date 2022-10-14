17968707455da8b80d288468962d66ede3ccb972
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-14 17:09:21 -0400

    Fixed URL pathing error, watchlist URL is responsive

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('user_watchlist/', include('watchlist.urls')),
+    path('api/watchlist/', include('watchlist.urls')),
 ]
--- backend/watchlist/urls.py
+++ backend/watchlist/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from watchlist import views
 
-urlpatters = [
+urlpatterns = [
     path('', views.user_watchlist)
 ]
