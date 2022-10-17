5e78df156abdd66b2143f4b05cff7619a8d3ad82
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-17 13:04:01 -0400

    Added abaility to GET, PUT, DELETE from a users watchlist + Added pathing for add to watchlist and trying to get target price decimal field correct

--- backend/watchlist/urls.py
+++ backend/watchlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from watchlist import views
 
 urlpatterns = [
-    path('', views.user_watchlist)
+    path('', views.user_watchlist),
+    path('add_to_watchlist/<pk>/', views.add_to_watchlist)
 ]
--- backend/watchlist/views.py
+++ backend/watchlist/views.py
@@ -1,17 +1,20 @@
+from urllib import request
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
+
 from .models import Watchlist
 from .serializers import WatchlistSerializer
 
 # Create your views here.
 
+
+
 @api_view(['GET', 'POST', 'DELETE'])
 @permission_classes([IsAuthenticated])
-def user_watchlist(request, pk):
-    watchlist = get_object_or_404(Watchlist, pk=pk)
+def user_watchlist(request):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
     if request.method == 'GET':
@@ -19,7 +22,25 @@ def user_watchlist(request, pk):
         serializer = WatchlistSerializer(stocks, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        serializer = WatchlistSerializer(watchlist, data=request.data)
+        serializer = WatchlistSerializer(Watchlist, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        Watchlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def add_to_watchlist(request, pk):
+    watchlist = get_object_or_404(Watchlist, pk=pk)
+    if request.method == 'GET':
+        stocks = Watchlist.objects.filter(user_id=request.user.id)
+        serializer = WatchlistSerializer(stocks, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = WatchlistSerializer(Watchlist, data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_200_OK)
