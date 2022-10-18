a542016f633b6522ad4a30ceaa270930b11bf5c8
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-18 17:18:36 -0400

    Added axios access to search for a stock on the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 // General Imports
 import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 import "./App.css";
 
 // Pages Imports
@@ -13,8 +14,25 @@ import Footer from "./components/Footer/Footer";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
+// import Watchlist from "./pages/Watchlist/watchlist";
+import axios from "axios";
 
 function App() {
+
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetchSearchedStock()
+  }, [])
+
+  const fetchSearchedStock = async () => {
+    let response = await axios.get('https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/AAPL,MSFT', { headers: {
+    'X-RapidAPI-Key': '86d3b4a83bmsh0dd08eec6709231p1c4988jsn55fac02dce50',
+    'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'
+  }
+})
+    setStocks(response.data)
+}
   return (
     <div>
       <Navbar />
@@ -23,16 +41,39 @@ function App() {
           path="/"
           element={
             <PrivateRoute>
-              <HomePage />
+              <HomePage HomePageProperties={stocks} />
             </PrivateRoute>
           }
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/watchlist" element={<Watchlist />} /> */}
       </Routes>
+          <div className="search">
+            <input type="text" className="searchTerm" placeholder="Music Library Search"></input>
+            <button type='submit' className="searchButton">
+            <i className="fa-fa search">search</i>
+            </button>
+          </div>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/AAPL,MSFT',
+  // headers: {
+  //   'X-RapidAPI-Key': '86d3b4a83bmsh0dd08eec6709231p1c4988jsn55fac02dce50',
+  //   'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'
+  // }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
