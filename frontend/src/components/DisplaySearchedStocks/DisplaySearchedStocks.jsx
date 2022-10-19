fbf407acf60da1ca2c21f961c0a3cfcb516a306f
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-19 17:05:09 -0400

    Changed the page name and navbar title to reflect my stock app

--- frontend/src/components/DisplaySearchedStocks/DisplaySearchedStocks.jsx
+++ frontend/src/components/DisplaySearchedStocks/DisplaySearchedStocks.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 
-export default function DisplaySearchedStocks(props){
+const DisplaySearchedStocks = () => {
+    const [displayStock, setStockDisplay] = useState([]);
 
-    function handleClick(stock){
+    useEffect(() =>{
 
-        console.log(stock)
-        let Id = stock.id.Id
+    }, [])
+    async function handleSubmit(event){
+        event.preventDefault()
+
+        let response = await axios.get('https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/', { headers: {
+            'X-RapidAPI-Key': '86d3b4a83bmsh0dd08eec6709231p1c4988jsn55fac02dce50',
+            'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'}})
+            setStockDisplay(response.data)
+            console.log("Searched Stock", displayStock)
     }
-    
 }
+
+export default DisplaySearchedStocks;
--- frontend/src/components/StockSearch/StockSearch.jsx
+++ frontend/src/components/StockSearch/StockSearch.jsx
@@ -17,7 +17,7 @@ const Search = () => {
     setSearchInput(searchTerm)
   }
   const fetchSearchResults = async () => {
-    let response = await axios.get('https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/%7Bsymbol%7D', { headers: {
+    let response = await axios.get('https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/', { headers: {
         'X-RapidAPI-Key': '86d3b4a83bmsh0dd08eec6709231p1c4988jsn55fac02dce50',
         'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'}})
     setSearchStocks(response.data)
