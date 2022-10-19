f83114f286b99196d96fc52e267e537891723607
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-19 15:25:20 -0400

    Attempting how to let the user use app without having sercret key displayed + getting API to be responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
 
   const fetchSearchedStock = async () => {
     let response = await axios.get('https://yahoo-finance15.p.rapidapi.com/api/yahoo/ne/news', { headers: {
-      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Key': '86d3b4a83bmsh0dd08eec6709231p1c4988jsn55fac02dce50',
       'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'
     }
   }
--- frontend/src/components/StockSearch/StockSearch.jsx
+++ frontend/src/components/StockSearch/StockSearch.jsx
@@ -21,7 +21,7 @@ const Search = () => {
         'X-RapidAPI-Key': '86d3b4a83bmsh0dd08eec6709231p1c4988jsn55fac02dce50',
         'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'}})
     setSearchStocks(response.data)
-    console.log("Stock", searchStocks)
+    console.log("Stock", stocks)
   }
 
   
