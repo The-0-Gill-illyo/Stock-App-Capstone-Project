a85063fddbe2acb1c2ce0aff0b0bf924a177210e
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-22 12:15:36 -0400

    Added Add Stock to the Watchlist folder + Modidified Watchlist file to visualize stocks added to watchlist

---
+++ frontend/src/components/Watchlist/AddStock.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import useCustomForm from "../../hooks/useCustomForm";
+
+// let initalValues = {
+//     id: "",
+//     watchlist: "",
+//     target_price: "",
+//     user_id: "",
+// };
+
+const AddStock = () =>{
+//     const [user, token] = useAuth{}
+//     const navigate = useNavigate{}
+//     const [formData, handleInputChange, handleSubmit] = useCustomForm(initalValues)
+
+
+
+}
+
+export default AddStock;
--- frontend/src/components/Watchlist/Watchlist.jsx
+++ frontend/src/components/Watchlist/Watchlist.jsx
@@ -1,22 +1,25 @@
+import React, { useState } from 'react';
 
 
-const Watchlist = (props) =>{
+const Watchlist = () =>{
+
+    const [entries, setEntries] = useState([{id: 1,stock_name: "AAPL", target_price: 100.50, user_id: 1}])
     return (
         <table>
             <thead>
             <tr>
                 <th>Id</th>
-                <th>Watchlist</th>
+                <th>Sock Name</th>
                 <th>Target Price</th>
                 <th>User Id</th>
             </tr>
             </thead>
             <tbody>
-            {props.parentWatchlist.map((stock, ...index) => {
+            {entries.map((stock, ...index) => {
                 return (
                     <tr key={index}>
                         <td>{stock.id}</td>
-                        <td>{stock.watchlist}</td>
+                        <td>{stock.stock_name}</td>
                         <td>{stock.target_price}</td>
                         <td>{stock.user_id}</td>
                     </tr>
